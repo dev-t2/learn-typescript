099eded613bc05a6d2dc5db17872dbb2eadd612d
t2 <oo_ri@naver.com>
2021-04-26 01:04:17 +0900

    App Icon, Splash Create and App Loading Update

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
-import React, { memo } from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, { memo, useCallback, useState } from 'react';
 import { View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading';
+
+import { cacheFonts, cacheImages } from './src/utils/cache';
 
 const App = () => {
-  return (
+  const [isReady, setIsReady] = useState(false);
+
+  const startAsync = useCallback(async () => {
+    const imageAssets = cacheImages([require('./assets/splash.png')]);
+    const fontAssets = cacheFonts([]);
+
+    await Promise.all([...imageAssets, ...fontAssets]);
+  }, []);
+
+  const onFinish = useCallback(() => {
+    setIsReady(true);
+  }, []);
+
+  return isReady ? (
     <View>
       <StatusBar style="dark" />
     </View>
+  ) : (
+    <AppLoading
+      startAsync={startAsync}
+      onFinish={onFinish}
+      onError={console.error}
+    />
   );
 };
 
