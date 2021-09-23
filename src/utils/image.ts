ba940e0827e2ac6156d4cff14989e7ad8810d37d
t2 <oo_ri@naver.com>
2021-09-23 23:30:16 +0900

    Reset Repository

--- src/utils/image.ts
+++
@@ -1,7 +0,0 @@
-const prefix =
-  'https://firebasestorage.googleapis.com/v0/b/simple-chat-c6059.appspot.com/o';
-
-export default {
-  logo: `${prefix}/logo.png?alt=media`,
-  photo: `${prefix}/photo.png?alt=media`,
-};
