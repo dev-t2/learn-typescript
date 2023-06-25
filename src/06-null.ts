ae598c3934ef82530c9f32a7e6825a399a485925
dev-t2 <oo_ri@naver.com>
2023-06-25 22:48:14 +0900

    06. Null and 07. Undefined Update

---
+++ src/06-null.ts
@@ -0,0 +1,9 @@
+const n = null;
+
+console.log({ n });
+
+console.log(typeof n);
+
+const unionNull: boolean | null = null;
+
+console.log({ unionNull });
---
+++ src/07-undefined.ts
@@ -0,0 +1,9 @@
+const u = undefined;
+
+console.log({ u });
+
+console.log(typeof u);
+
+const unionUndefined: boolean | undefined = undefined;
+
+console.log({ unionUndefined });
