7447f74b76e774d91f24109f2939d4e46abab8b9
dev-t2 <oo_ri@naver.com>
2023-07-01 15:37:28 +0900

    14. Void Update

---
+++ src/14-void.ts
@@ -0,0 +1,21 @@
+const getMessage = (message: string) => {
+  console.log(message);
+};
+
+// const getMessage = (message: string): void => {
+//   console.log(message);
+// };
+
+// const getMessage = (message: string) => {
+//   console.log(message);
+
+//   return;
+// };
+
+// const getMessage = (message: string): void => {
+//   console.log(message);
+
+//   return undefined;
+// };
+
+// const value: number = getMessage('message');
