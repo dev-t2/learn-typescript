bca192bf2ff029c674d05750d1f90afdf764116e
dev-t2 <oo_ri@naver.com>
2023-07-01 17:58:33 +0900

    05. Boolean Update

---
+++ src/05-boolean.ts
@@ -0,0 +1,5 @@
+let bool = false;
+
+console.log({ bool });
+
+// bool = new Boolean(true);
--- src/20-boolean.ts
+++
@@ -1,9 +0,0 @@
-// let isSuccess: boolean = false;
-
-let isSuccess = false;
-
-isSuccess = true;
-
-console.log(typeof isSuccess);
-
-// isSuccess = new Boolean(false);
