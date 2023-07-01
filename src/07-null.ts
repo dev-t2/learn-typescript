f5ee388ff5a8dd4b538a6c71a4571cc630ccc5c9
dev-t2 <oo_ri@naver.com>
2023-07-01 18:00:36 +0900

    07. Null Update

---
+++ src/07-null.ts
@@ -0,0 +1,9 @@
+const value1 = null;
+
+console.log({ value1 });
+
+console.log(typeof value1);
+
+const value2: boolean | null = null;
+
+console.log({ value2 });
--- src/23-null.ts
+++
@@ -1,9 +0,0 @@
-const n = null;
-
-console.log({ n });
-
-console.log(typeof n);
-
-const unionNull: boolean | null = null;
-
-console.log({ unionNull });
