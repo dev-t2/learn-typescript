23cf65e46343e0fe1f618100b54785214f679d22
dev-t2 <oo_ri@naver.com>
2023-07-01 17:59:35 +0900

    06. Undefined Update

---
+++ src/06-undefined.ts
@@ -0,0 +1,9 @@
+const value1 = undefined;
+
+console.log({ value1 });
+
+console.log(typeof value1);
+
+const value2: boolean | undefined = undefined;
+
+console.log({ value2 });
--- src/24-undefined.ts
+++
@@ -1,9 +0,0 @@
-const u = undefined;
-
-console.log({ u });
-
-console.log(typeof u);
-
-const unionUndefined: boolean | undefined = undefined;
-
-console.log({ unionUndefined });
